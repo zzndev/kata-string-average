ea4d4718143644fd10b03760a2692e5ff28be3ff
Zaw Zaw Naing <zawzaw@Zaws-MacBook-Air.local>
2020-01-29 17:01:04 +0800

    Fail empty function of averageString

---
+++ string-average.test.js
@@ -0,0 +1,7 @@
+const stringAverage = require('./string-average');
+
+describe('String Average', () => {
+    it('should be a function', () => {
+        expect(typeof stringAverage).toBe('function');
+    });
+});
